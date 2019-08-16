4b6b21966006256ababa5e95e4a4b09bec1b0cfa
cimnode <49315989+cimnode@users.noreply.github.com>
2019-08-16 11:35:00 -0400

    Initial upload of CLI command scripts

---
+++ bin/cli_command.py
@@ -0,0 +1,10 @@
+import os, sys
+
+if len(sys.argv) != 2 :
+  print "No command to execute passed."
+  sys.exit()
+
+command_to_run = sys.argv[1]
+print "Command: " + command_to_run
+os.system( command_to_run )
+
