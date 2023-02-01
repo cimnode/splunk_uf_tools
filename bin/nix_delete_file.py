22c29e5b05dab307b71a251b35a3517e70bd9f6e
cimnode <49315989+cimnode@users.noreply.github.com>
2023-02-01 14:01:24 -0500

    Update nix_delete_file.py

--- bin/nix_delete_file.py
+++ bin/nix_delete_file.py
@@ -1,23 +1,23 @@
 import os, sys
 
 if len(sys.argv) != 3 :
-  print "Parameters not correct. 1. File to confirm, 2. File to delete"
+  print( "Parameters not correct. 1. File to confirm, 2. File to delete" )
   sys.exit()
 
 file_to_confirm = sys.argv[1]
 file_to_delete = sys.argv[2]
 
-print "File to confirm: " + file_to_confirm
-print "File to delete: " + file_to_delete
+print( "File to confirm: " + file_to_confirm )
+print( "File to delete: " + file_to_delete )
 
 if os.path.isfile( file_to_confirm ) :
   if os.path.isfile( file_to_delete ) :
     os.remove( file_to_delete )
     if os.path.isfile( file_to_delete ) :
-      print "Error. File not removed."
+      print( "Error. File not removed." )
     else:
-      print "File deleted."
+      print( "File deleted." )
   else:
-    print "File to delete not found."
+    print( "File to delete not found." )
 else:
-  print "File to confirm not found."
+  print( "File to confirm not found." )
