09e9fdc06dad44c9d0b62ab3bf13321e62b7e2ca
cimnode <49315989+cimnode@users.noreply.github.com>
2023-02-01 14:00:26 -0500

    Update cli_command.py

--- bin/cli_command.py
+++ bin/cli_command.py
@@ -1,10 +1,10 @@
 import os, sys
 
 if len(sys.argv) != 2 :
-  print "No command to execute passed."
+  print( "No command to execute passed." )
   sys.exit()
 
 command_to_run = sys.argv[1]
-print "Command: " + command_to_run
+print( "Command: " + command_to_run )
 os.system( command_to_run )
 
